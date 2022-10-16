4cb227dd48bd65db9276c985c8a09d61ce57d204
Thomas Barlow <github@mysterymayhem.co.uk>
2022-10-16 17:44:46 +0100

    Add MMD shapes import/export
    Add extra UI controls for adding/moving MMD mappings
    Add special handling for comments in MMD mappings

--- ui_mmd_shapes.py
+++ ui_mmd_shapes.py
@@ -1,4 +1,6 @@
-from bpy.types import Panel, Operator, UIList, Context, UILayout, Mesh
+from bpy.types import Panel, Operator, UIList, Context, UILayout, Mesh, Menu
+from bpy.props import BoolProperty
+from bpy_extras.io_utils import ImportHelper, ExportHelper
 
 from . import cats_translate
 from .extensions import ScenePropertyGroup, MmdShapeMapping, MmdShapeMappingGroup
@@ -10,6 +12,11 @@ from .context_collection_ops import (
     ContextCollectionOperatorBase,
     PropCollectionType,
 )
+
+
+# Not using commas because they can be easily used in shape key names. Technically tabs can too via scripting, but they
+# show up as boxes in Blender's UI
+CSV_DELIMITER = '\t'
 
 
 class MmdMappingList(UIList):
@@ -26,25 +33,34 @@ class MmdMappingList(UIList):
                 temp_shape_keys = linked_mesh.shape_keys
                 if temp_shape_keys:
                     shape_keys = temp_shape_keys
-
-        row = layout.row(align=True)
-        row.label(text="", icon='DECORATE')
-        column_flow = row.column_flow(columns=3, align=True)
-        if shape_keys:
-            # Annoyingly, we can't get rid of the icon, only replace it with a different one
-            column_flow.prop_search(item, 'model_shape', shape_keys, 'key_blocks', text="")
+        # TODO: Replace the '#' prefix with a customisable StringProperty in-case users want to use shape keys starting
+        #  with the default of '#'
+        model_shape = item.model_shape
+        if model_shape.startswith('#'):
+            layout.prop(item, 'model_shape', emboss=False, text="")
+            # The row ends up a slightly different height to non-comment rows if we don't put something in a column_flow
+            column_flow = layout.column_flow(columns=1, align=True)
+            column_flow.label(text="")
         else:
-            column_flow.prop(item, 'model_shape', text="")
-        column_flow.prop(item, 'mmd_name', text="")
-        cats_row = column_flow.row(align=True)
-        cats_row.prop(item, 'cats_translation_name', text="")
-        op_row = cats_row.row(align=True)
-        op_row.enabled = bool(item.mmd_name)
-        translate_options = op_row.operator(cats_translate.CatsTranslate.bl_idname, text="", icon='WORLD_DATA')
-        translate_options.to_translate = item.mmd_name
-        translate_options.is_shape_key = True
-        # Path from context to the property
-        translate_options.data_path = 'scene.' + item.path_from_id('cats_translation_name')
+            row = layout.row(align=True)
+            row.label(text="", icon='DECORATE')
+            column_flow = row.column_flow(columns=3, align=True)
+            if shape_keys:
+                # Annoyingly, we can't get rid of the icon, only replace it with a different one
+                column_flow.prop_search(item, 'model_shape', shape_keys, 'key_blocks', text="")
+            else:
+                column_flow.prop(item, 'model_shape', text="")
+            column_flow.prop(item, 'mmd_name', text="")
+            cats_row = column_flow.row(align=True)
+            cats_row.prop(item, 'cats_translation_name', text="", emboss=False)
+            op_row = cats_row.row(align=True)
+            op_row.enabled = bool(item.mmd_name)
+            translate_options = op_row.operator(cats_translate.CatsTranslate.bl_idname, text="", icon='WORLD_DATA')
+            translate_options.to_translate = item.mmd_name
+            translate_options.is_shape_key = True
+            # Path from context to the property
+            translate_options.data_path = 'scene.' + item.path_from_id('cats_translation_name')
+            translate_options.custom_description = "Translate the MMD shape key"
 
 
 class MmdMappingControlBase(ContextCollectionOperatorBase):
@@ -76,6 +92,18 @@ class MmdMappingMove(MmdMappingControlBase, CollectionMoveBase):
     bl_idname = 'mmd_shape_mapping_move'
 
 
+class MmdShapesAddMenu(Menu):
+    bl_idname = "mmd_shape_mapping_add"
+    bl_label = "Add"
+
+    def draw(self, context: Context):
+        layout = self.layout
+        layout.operator(MmdMappingAdd.bl_idname, text="", icon='TRIA_UP_BAR').position = 'START'
+        layout.operator(MmdMappingAdd.bl_idname, text="", icon='TRIA_UP').position = 'BEFORE'
+        layout.operator(MmdMappingAdd.bl_idname, text="", icon='TRIA_DOWN').position = 'AFTER'
+        layout.operator(MmdMappingAdd.bl_idname, text="", icon='TRIA_DOWN_BAR').position = 'END'
+
+
 class LoadDefaults(Operator):
     """Load default mmd shape data."""
     bl_idname = "mmd_shapes_load_defaults"
@@ -86,23 +114,74 @@ class LoadDefaults(Operator):
         return {'FINISHED'}
 
 
-class ExportShapeSettings(Operator):
-    """Export a .csv containing mmd shape data"""
+class ExportShapeSettings(Operator, ExportHelper):
+    """Export a .csv (tab separated) containing mmd shape data"""
     bl_idname = "mmd_shapes_export"
     bl_label = "Export Mappings"
     bl_options = {'UNDO'}
 
+    filename_ext = ".csv"
+
     def execute(self, context: Context) -> set[str]:
+        mappings = ScenePropertyGroup.get_group(context.scene).mmd_shape_mapping_group.mmd_shape_mappings
+        lines = []
+        for mapping in mappings:
+            model_shape = mapping.model_shape
+            mmd_name = mapping.mmd_name
+            cats_translation_name = mapping.cats_translation_name
+            if CSV_DELIMITER in model_shape:
+                self.report({'WARNING'}, f"The .csv delimiter '{CSV_DELIMITER}' was found in the Shape Key"
+                                         f" '{model_shape}', it has been removed in the export")
+                model_shape = model_shape.replace(CSV_DELIMITER, "")
+            if CSV_DELIMITER in mmd_name:
+                self.report({'WARNING'}, f"The .csv delimiter '{CSV_DELIMITER}' was found in the MMD name"
+                                         f" '{mmd_name}', it has been removed in the export")
+                mmd_name = mmd_name.replace(CSV_DELIMITER, "")
+            if CSV_DELIMITER in cats_translation_name:
+                self.report({'WARNING'}, f"The .csv delimiter '{CSV_DELIMITER}' was found in the Cats translation name"
+                                         f" '{cats_translation_name}', it has been removed in the export")
+                cats_translation_name = cats_translation_name.replace(CSV_DELIMITER, "")
+            lines.append(model_shape + "\t" + mmd_name + "\t" + cats_translation_name)
+        file = open(self.filepath, 'w')
+        # Python's writelines doesn't even write lines, have to add the newlines yourself...
+        file.writelines(line + "\n" for line in lines)
+        file.close()
         return {'FINISHED'}
 
 
-class ImportShapeSettings(Operator):
-    """Import a .csv containing mmd shape data"""
+class ImportShapeSettings(Operator, ImportHelper):
+    """Import a .csv (tab separated) containing mmd shape data"""
     bl_idname = "mmd_shapes_import"
     bl_label = "Import Mappings"
     bl_options = {'UNDO'}
 
+    add: BoolProperty(
+        name="Add",
+        description="Add the imported mappings to the list without clearing the list beforehand"
+    )
+
     def execute(self, context: Context) -> set[str]:
+        file = open(self.filepath, 'r')
+        lines = file.readlines()
+        parsed_lines: list[tuple[str, str, str]] = []
+        for line_no, line in enumerate(lines, start=1):
+            split = line.split(CSV_DELIMITER)
+            num_fields = len(split)
+            expected_fields = 3
+            if num_fields >= 3:
+                # Extra fields may be used for comments in the file
+                parsed_lines.append((split[0], split[1], split[2]))
+            else:
+                self.report({'WARNING'}, f"Failed to parse line {line_no}, got {num_fields} fields, but expected"
+                                         f" at least {expected_fields}")
+        mappings = ScenePropertyGroup.get_group(context.scene).mmd_shape_mapping_group.mmd_shape_mappings
+        if not self.add:
+            mappings.clear()
+        for shape_name, mmd_name, cats_name in parsed_lines:
+            added = mappings.add()
+            added.model_shape = shape_name
+            added.mmd_name = mmd_name
+            added.cats_translation_name = cats_name
         return {'FINISHED'}
 
 
@@ -118,7 +197,16 @@ class CatsTranslateAll(Operator):
 
     def execute(self, context: Context) -> set[str]:
         mappings = ScenePropertyGroup.get_group(context.scene).mmd_shape_mapping_group.mmd_shape_mappings
-        to_translate = [m.mmd_name for m in mappings if m.mmd_name]
+
+        # Get all mmd_names that are non-empty and filter out any duplicates
+        unique_to_translate = set()
+        to_translate = []
+        for mapping in mappings:
+            mmd_name = mapping.mmd_name
+            if mmd_name not in unique_to_translate:
+                unique_to_translate.add(mmd_name)
+                to_translate.append(mmd_name)
+
         translations = cats_translate.cats_translate(to_translate, is_shape_key=True, calling_op=self)
         if translations:
             for mapping in mappings:
@@ -173,11 +261,16 @@ class MmdShapeMappingsPanel(Panel):
 
         # Add list controls vertically
         vertical_buttons_col = list_controls_col.column(align=True)
-        vertical_buttons_col.operator(MmdMappingAdd.bl_idname, text="", icon="ADD")
+        vertical_buttons_col.operator_menu_hold(MmdMappingAdd.bl_idname, text="", icon="ADD", menu=MmdShapesAddMenu.bl_idname)
         vertical_buttons_col.operator(MmdMappingRemove.bl_idname, text="", icon="REMOVE")
         vertical_buttons_col.separator()
+        vertical_buttons_col.operator(MmdMappingMove.bl_idname, text="", icon="TRIA_UP_BAR").type = 'TOP'
         vertical_buttons_col.operator(MmdMappingMove.bl_idname, text="", icon="TRIA_UP").type = 'UP'
         vertical_buttons_col.operator(MmdMappingMove.bl_idname, text="", icon="TRIA_DOWN").type = 'DOWN'
+        vertical_buttons_col.operator(MmdMappingMove.bl_idname, text="", icon="TRIA_DOWN_BAR").type = 'BOTTOM'
+        vertical_buttons_col.separator()
+        vertical_buttons_col.operator(ImportShapeSettings.bl_idname, text="", icon="IMPORT")
+        vertical_buttons_col.operator(ExportShapeSettings.bl_idname, text="", icon="EXPORT")
 
         if not cats_translate.cats_exists():
             col.label(text="Cats addon not found")
