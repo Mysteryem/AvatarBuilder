ff584bee7cb3625a4e47af9592791dcd9046af6a
Thomas Barlow <github@mysterymayhem.co.uk>
2022-11-21 06:04:10 +0000

    Fix missing return for modifier_mask gret integration

--- integration_gret.py
+++ integration_gret.py
@@ -1,23 +1,20 @@
 import bpy
 from bpy.types import Object
 
+from typing import Union, Literal
+
 from . import utils
 
 
-# TODO: Rename to something more like 'get_gret_version' and instead get gret from the addons and look at its version
-def check_gret_shape_key_apply_modifiers():
-    """Returns a truthy value when valid.
-     Returns None when not detected.
-     Returns False when detected, but the version could not be determined."""
+def check_gret_shape_key_apply_modifiers() -> Union[str, Literal[False], None]:
+    """When valid, returns a truthy string that also indicates version.
+     When not detected, returns None.
+     When detected, but the version could not be determined, returns False."""
     if hasattr(bpy.ops, 'gret') and hasattr(bpy.ops.gret, 'shape_key_apply_modifiers'):
         operator = bpy.ops.gret.shape_key_apply_modifiers
         for prop in operator.get_rna_type().properties:
             identifier = prop.identifier
-            if identifier == 'modifier_mask':
-                # Not yet implemented
-                # return identifier
-                return False
-            elif identifier == 'keep_modifiers':
+            if identifier == 'modifier_mask' or identifier == 'keep_modifiers':
                 return identifier
         return False
     return None
